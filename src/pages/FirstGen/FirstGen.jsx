1e557b87d01548eed3765ed07bc071a505e9c7ea
Lukas Kliukas <lukaskliukas@gmail.com>
2022-08-18 11:57:57 +0300

    Grid added

--- src/pages/FirstGen/FirstGen.jsx
+++ src/pages/FirstGen/FirstGen.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import * as S from './FirstGen.style';
 
 const FirstGen = () => {
-  return <div>FirstGen</div>;
+  return (
+    <S.Wrapper>
+      <S.Grid>
+        <S.Image src='\images\Avensis1_front.jpg' alt='' />
+        <S.Image src='\images\Avensis1_interior.jpg' alt='' />
+      </S.Grid>
+    </S.Wrapper>
+  );
 };
 
 export default FirstGen;
---
+++ src/pages/FirstGen/FirstGen.style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Image = styled.img`
+  width: 30rem;
+`;
+export const Grid = styled.div`
+  display: grid;
+  align-items: center;
+  grid-template-columns: 1fr 1fr;
+`;
+export const Wrapper = styled.div`
+  /* background: #3e94f4; */
+`;
