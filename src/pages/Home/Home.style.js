5518bd446101272ac413c365bfabb361ad234260
Lukas Kliukas <lukaskliukas@gmail.com>
2022-08-19 15:32:43 +0300

    Style added for avensis logo

--- src/pages/Home/Home.style.js
+++ src/pages/Home/Home.style.js
@@ -4,3 +4,15 @@ export const Wrapper = styled.div`
   background: #3e94f4;
   height: 40rem;
 `;
+export const Header = styled.h1`
+  background: #3e94f4;
+  margin: 0rem;
+  max-width: 40rem;
+  padding: 4rem 4rem;
+`;
+export const logoImg = styled.img`
+  border-radius: 2rem;
+  box-shadow: 4px 4px 4px #d8d4d3;
+  margin: auto;
+  display: block;
+`;
