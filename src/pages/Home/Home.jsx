cb3582796316190c5dd10f6694c5d0fc12477933
Lukas Kliukas <lukaskliukas@gmail.com>
2022-08-19 15:32:29 +0300

    Little bit of information added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 import * as S from './Home.style';
 
 const Home = () => {
-  return <S.Wrapper>Home page bla bla bla</S.Wrapper>;
+  return (
+    <S.Wrapper>
+      <S.Header>
+        Hello, this page is made about Toyota Avensis cars. Here you can find
+        information about all three released genders of Avensis automobiles.
+      </S.Header>
+      <S.logoImg src='images\Avensis_logo.jpg' alt='avensis_logo' />
+    </S.Wrapper>
+  );
 };
 
 export default Home;
