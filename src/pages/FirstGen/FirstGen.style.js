c652a656469646196524c76279fcf4dbf9959e0d
Lukas Kliukas <lukaskliukas@gmail.com>
2022-08-19 15:36:01 +0300

    Few style things eddited

--- src/pages/FirstGen/FirstGen.style.js
+++ src/pages/FirstGen/FirstGen.style.js
@@ -2,12 +2,14 @@ import styled from 'styled-components';
 
 export const Image = styled.img`
   width: 30rem;
+  border-radius: 2rem;
 `;
 export const Grid = styled.div`
+  padding: 2rem 2rem;
   display: grid;
   align-items: center;
   grid-template-columns: 1fr 1fr;
 `;
 export const Wrapper = styled.div`
-  /* background: #3e94f4; */
+  background: #3e94f4;
 `;
