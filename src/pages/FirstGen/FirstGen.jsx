d2dec8bc0b87d0b0af039179fd4a8c6f66127732
Lukas Kliukas <lukaskliukas@gmail.com>
2022-08-22 15:44:23 +0300

    Info text added with minimum style

--- src/pages/FirstGen/FirstGen.jsx
+++ src/pages/FirstGen/FirstGen.jsx
@@ -7,6 +7,29 @@ const FirstGen = () => {
       <S.Grid>
         <S.Image src='\images\Avensis1_front.jpg' alt='' />
         <S.Image src='\images\Avensis1_interior.jpg' alt='' />
+        <S.Info>
+          Toyota introduced the Avensis nameplate on its new model, which was
+          launched at the end of 1997. Although an extensive revamp of the
+          previous generation Carina E, the only main common parts were the
+          engines, with an all new style of body. The car was built at the
+          Burnaston factory in Derby. At the same time, production of the
+          five-door Toyota Corolla also started at the British plant. The first
+          generation Avensis had the option of four engines (1.6-, 1.8- and
+          2.0-litre petrol and a 2.0-litre turbo-diesel) and three body styles
+          (saloon, hatchback and estate). The station wagon was essentially the
+          Japanese-market second generation Toyota Caldina. All of the range
+          gave solid build quality, excellent reliability, a spacious
+          comfortable interior, generous equipment, smooth ride quality and good
+          refinement, but very little in the way of excitement. The first
+          generation Avensis received a facelift in August 2000. The engines
+          were fitted with variable valve timing, satellite navigation was
+          included as a standard feature along the range and the sporty Avensis
+          2.0 L SR (with body kit and tuned suspension) was added but overall
+          sales remained low. The first generation Avensis was also sold in
+          South America. In Argentina and Brazil, it was sold as the Toyota
+          Corona (as seen on brochures), but in Chile and Peru, it was sold as
+          the Corona Avensis.
+        </S.Info>
       </S.Grid>
     </S.Wrapper>
   );
--- src/pages/FirstGen/FirstGen.style.js
+++ src/pages/FirstGen/FirstGen.style.js
@@ -7,9 +7,16 @@ export const Image = styled.img`
 export const Grid = styled.div`
   padding: 2rem 2rem;
   display: grid;
+  gap: 2rem;
   align-items: center;
   grid-template-columns: 1fr 1fr;
 `;
 export const Wrapper = styled.div`
   background: #3e94f4;
+`;
+export const Info = styled.div`
+  background: #d8d4d3;
+  padding: 2rem;
+  border-radius: 2rem;
+  width: (10rem - 4rem);
 `;
