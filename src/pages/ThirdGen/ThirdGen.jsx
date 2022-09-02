dcac70a7862c88774b60edcc8dd87ae6224783f5
Lukas Kliukas <lukaskliukas@gmail.com>
2022-09-02 16:39:58 +0300

    Third Gen info added

---
+++ src/pages/ThirdGen/ThirdGen.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import * as S from './ThirdGen.style';
+
+const ThirdGen = () => {
+  return (
+    <S.Wrapper>
+      <S.Grid>
+        <S.Image src='\images\Avensis3_front.jpg' alt='' />
+        <S.Image src='\images\Avensis3_interior.jpg' alt='' />
+        <S.Info>
+          The third generation Avensis was revealed at the 2008 Paris Motor
+          Show. It went on sale in January 2009. Like the previous model, it is
+          built at Toyota's Burnaston plant in Derbyshire, England. This
+          generation is available in saloon or estate styles only.[8] The third
+          generation Avensis full-year sales goal was of 115,000 vehicles.[9]
+          Toyota ED2 initially designed the Avensis giving it a more sleek and
+          athletic stance. It has a drag coefficient of Cd=0.28 for the saloon
+          and Cd=0.29 for the wagon body. Other design goals were to create the
+          most comfortable suspension in its class while still offering sporty
+          handling. The Avensis continued to use front MacPherson struts and
+          rear double wishbones. Unlike the previous generation Avensis models,
+          there is no five door hatchback body style in this lineup, in favour
+          of saloon and estate body styles. Major options include bi xenon HID
+          headlamps, Adaptive Front-lighting System (AFS), and adaptive cruise
+          control. The Avensis wagon began being imported again to Japan in
+          mid-2011, with 2.0-litre 4-cylinder engine and CVT transmission in Li
+          and Xi variants.[10] The Avensis wagon was discontinued in Japan on 27
+          April 2018.
+        </S.Info>
+      </S.Grid>
+    </S.Wrapper>
+  );
+};
+
+export default ThirdGen;
