271b60b87e1c5822d2d7e39beb42fcc4a45d1cd2
Lukas Kliukas <lukaskliukas@gmail.com>
2022-09-04 18:20:44 +0300

    Responsive design

--- src/components/Header/Header.style.js
+++ src/components/Header/Header.style.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 export const Header = styled.div`
+  @media only screen and (max-width: 1200px) {
+    flex-direction: column;
+  }
   /* margin-bottom: 4rem; */
   top: 0;
   width: 100%;
@@ -12,9 +15,15 @@ export const Header = styled.div`
   color: #3e94f4;
 `;
 export const LogoImg = styled.img`
+  @media only screen and (max-width: 1200px) {
+    width: 4rem;
+  }
   width: 8rem;
 `;
 export const ALinks = styled.a`
+  @media only screen and (max-width: 1200px) {
+    font-size: 1rem;
+  }
   font-family: 'Rubik Dirt', cursive;
   font-size: 1.5rem;
   color: #000;
